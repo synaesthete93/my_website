8890e3aeeb7b5c430fbf5336917810bc2a887bd3
Radu Popescu <radu.popescu@maruhub.com>
2021-05-16 19:44:47 +0100

    Skeleton

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+// .prettierrc.js
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+};
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <h4>
+            Created from the Vue basic app (created with `vue-serve`), this app
+            features:
+        </h4>
+        <ul>
+            <li>The Vue Property Decorator syntax</li>
+            <li>Linter, prettifier, the works!</li>
+            <li>Vue router</li>
+            <li>Vuex store</li>
+        </ul>
+    </div>
 </template>
